f7c932b5777485a5e1b0b9b402ed7411c898d415
itsabhi018 <abhishekkukreja2003@gmail.com>
2025-07-31 00:44:50 +0530

    Added 4 different solutions to array:LC26,LC189,LC283,LC1752

---
+++ ARRAYS/LC26_DUPLICATES_IN_ARRAY.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int temp=nums[0];
+        int k=0;
+        for(int i=1;i<nums.size();i++){
+            if (nums[i]!=temp)
+        }
+        
+    }
+};
